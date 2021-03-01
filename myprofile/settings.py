92d385cabe4dbe687ff9020cd116675d8b546d62
LOREYLOLIM <emmanuellorey>
2021-03-01 12:14:58 -0800

    mythirdcommit

--- myprofile/settings.py
+++ myprofile/settings.py
@@ -28,14 +28,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
-
 
 # Application definition
 
