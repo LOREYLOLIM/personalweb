34290e365e579df653d464c7a0e01883cae5c45b
LOREYLOLIM <emmanuellorey>
2021-03-01 16:10:55 -0800

    myforthcommit

--- myprofile/settings.py
+++ myprofile/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # MEDIA_URL = '/media/'
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
